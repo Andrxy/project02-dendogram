556ae0d9d4e68f0ad632eb5d4979909e2796c8b3
Chona <gsanchezc0106@gmail.com>
2025-11-06 19:40:42 -0600

    Agregar mvc

---
+++ Project02-Dendogram/Presentation/ClusteringJerarquico.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project02_Dendogram.Presentation
+{
+    /// <summary>
+    /// Lógica de interacción para ClusteringJerarquico.xaml
+    /// </summary>
+    public partial class ClusteringJerarquico : Window
+    {
+        public ClusteringJerarquico()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
